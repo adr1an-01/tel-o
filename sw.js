36996f7f86ec52a37ad4dbc3644f6dcd95a84d98
adr1an-01 <adriandossantoshu@gmail.com>
2026-06-18 23:23:12 -0300

    v4: biblia navegavel, PWA, login lembrado

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='central-v12';
+const CACHE='central-v13';
 const ASSETS=['/','/style.css','/app.js','/manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>caches.open(CACHE)).then(c=>c.add(new Request('/biblia.json',{cache:'no-cache'})).catch(()=>{})));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
